7575d2503ded953d68b3f5a0a5fcc8b27a8799ad
Andrew Merrell and Blake Scala <blake@>
2016-08-10 09:03:04 -0700

    add js to tie user inputs to story.

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    var nounInput = $("input#noun").val();
+    var noun2Input = $("input#noun2").val();
+    var verbInput = $("input#verb").val();
+    var adjInput = $("input#adj").val();
+    var adverbInput = $("input#adverb").val();
+
+    $(".noun").text(nounInput);
+    $(".noun2").text(noun2Input);
+    $(".verb").text(verbInput);
+    $(".adj").text(adjInput);
+    $(".adverb").text(adverbInput);
+
+    $("#story").show();
+
+    event.preventDefault();
+  });
+
+});
